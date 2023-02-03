f0ff77ac236e32684e35d1b16608d419e65df136
Thingk0 <audtjd9080@naver.com>
2023-02-03 23:26:07 +0900

    modified: MySQL -> MariaDB

--- src/test/java/com/thingk0/shopping/repository/ItemRepositoryTest.java
+++ src/test/java/com/thingk0/shopping/repository/ItemRepositoryTest.java
@@ -2,7 +2,6 @@ package com.thingk0.shopping.repository;
 
 import com.thingk0.shopping.entity.Item;
 import com.thingk0.shopping.entity.ItemStatus;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
